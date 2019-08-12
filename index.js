e01eef1ce642ccec4e41dfee189038999a095435
jordanbacon <jmbacon92@gmail.com>
2019-08-12 09:38:34 -0700

    Done.

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const username = document.getElementById('name-input').value
+    const jokeList = document.getElementById('joke-list')
+    const newJokeLi = document.createElement('li')
 
     if(username === "") return;
     fetchJoke()
+
     newJokeLi.innerHTML = `
     <span class="username">${username} says:</span> ${joke}
     `
